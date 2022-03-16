3c576708f4a6f4331fb71d7245a2b5c7e77b3855
Gabriel Lopes <57074487+unifgabsantos@users.noreply.github.com>
2022-03-16 20:05:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from FEI import Bot,getAccounts
+from os import system
 def main():
     accounts = getAccounts()
     for account in accounts:
         Bot(account).start()
 main()
+system("pause")
